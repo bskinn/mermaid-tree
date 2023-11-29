23c7d664938ef4359cfe53409a43ef13a29e5089
Brian Skinn <brian.skinn@gmail.com>
2023-11-28 23:18:05 -0500

    Add some consts, enums, and types

---
+++ src/consts.ts
@@ -0,0 +1,19 @@
+import type { TNodeShapeDelims } from "./types"
+
+import { E_NodeShapes } from "./enums"
+
+export const MARKER_STRING_START = '<!-- mermaid-fs-diagram -->'
+export const MARKER_STRING_STOP = '<!-- mermaid-fs-diagram-stop -->'
+export const MARKER_REGEX_START = new RegExp(`^${MARKER_STRING_START}$`, 'm')
+export const MARKER_REGEX_STOP = new RegExp(`^${MARKER_STRING_STOP}$`, 'm')
+export const MARKER_REGEX_SPAN = new RegExp(
+  `${MARKER_STRING_START}[\\w\\W]+?${MARKER_STRING_STOP}`,
+)
+
+export const NODE_SHAPES: { [key in E_NodeShapes]: TNodeShapeDelims } = {
+  [E_NodeShapes.Square]: { open: '[', close: ']' },
+  [E_NodeShapes.Rounded]: { open: '(', close: ')' },
+}
+
+export const SRC_OPEN_LINE = '```mermaid'
+export const SRC_CLOSE_LINE = '```'
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,11 @@
-// Types!
+export type TDirName = string
+export type TFileName = string
+export type TDirContents = { dirs: Array<TDirName>; files: Array<TFileName> }
+export type TDirTreeNode = {
+  dirName: TDirName
+  nodeName: string
+  parents: Array<TDirName>
+  subdirs: Array<TDirTreeNode>
+  files: Array<TFileName>
+}
+export type TNodeShapeDelims = { open: string; close: string }
