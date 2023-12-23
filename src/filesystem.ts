cc30b58914f66ea35b057fc464d10206d13ee902
Brian Skinn <brian.skinn@gmail.com>
2023-12-23 00:03:00 -0500

    Add tree console-rendering functions

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -44,17 +44,75 @@ export const buildDirTree = (
 
   console.log(`Traversing ${fullPath}...`)
 
+  /*
+    It appears that node names in Mermaid are *case-sensitive*:
+      https://www.reddit.com/r/Notion/comments/rybwkx/notion_mermaid_gotchas_potential_pitfalls_that_i/
+    
+    Thus, by choosing the all-lowercase name 'root' for the name
+    for the root directory node, we shouldn't have to worry about
+    node name collisions if there happens to be a top-level
+    subdirectory named 'root'.
+  */
   return {
     parents: parents,
     dirName: dirName,
     nodeName:
       parents.length == 0
         ? 'root'
-        : pathArray
+        : pathArray.slice(1)
             .map((dir) => dir.replace(/[^0-9a-z]/gi, ''))
             .map((dir) => dir[0].toUpperCase() + dir.slice(1))
             .join(''),
     files: dirContents.files,
     subdirs: dirContents.dirs.map((dir) => buildDirTree(dir, pathArray)),
   }
+}
+
+/**
+ * Internal function for recursive traversal and printing of directory contents
+ *
+ * @param {TDirTreeNode} rootNode - Node at which to start/continue
+ *  traversal/printing
+ * @param {boolean} showFiles - If true, print each directory's file contents;
+ *  if false, print only the directories.
+ * @param {number} indentWidth - Number of spaces to indent each level of the
+ *  directory tree
+ * @param {number} indent - Current indentation width at this recursion depth
+ */
+const renderDirTreeInternal = (
+  rootNode: TDirTreeNode,
+  showFiles: boolean,
+  indentWidth: number = 2,
+  indent: number = 0,
+): void => {
+  const newIndent = indent + indentWidth
+
+  console.log(
+    ' '.repeat(indent) + rootNode.dirName + '/' + `  (${rootNode.nodeName})`,
+  )
+
+  if (showFiles) {
+    rootNode.files.forEach((f) => console.log(' '.repeat(newIndent) + '- ' + f))
+  }
+
+  rootNode.subdirs.forEach((sd) => {
+    renderDirTreeInternal(sd, showFiles, indentWidth, newIndent)
+  })
+}
+
+/**
+ * Print to console a formatted directory tree descending from the given root node
+ *
+ * @param {TDirTreeNode} rootNode - Node at which to start traversal/printing
+ * @param {boolean} showFiles - If true, print each directory's file contents;
+ *  if false, print only the directories.
+ * @param {number} indentWidth - Number of spaces to indent each level of the
+ *  directory tree
+ */
+export const renderDirTree = (
+  rootNode: TDirTreeNode,
+  showFiles: boolean = true,
+  indentWidth: number = 2,
+): void => {
+  renderDirTreeInternal(rootNode, showFiles, indentWidth, 0)
 }
